e65abdcb0c372a80b359ade91c9d030b2ca35bd3
ThatIsAUsername <ThatIsAUsername@noreply.github.com>
2021-12-31 17:52:50 -0600

    Timeview.contains() now handles references nad records.

--- data_types/timeview.py
+++ data_types/timeview.py
@@ -1,7 +1,8 @@
 
-from datetime import date, timedelta
+from typing import Union
 
-from data_types import Timeline
+from datetime import date
+from data_types import Timeline, TimeReference, EventRecord
 
 
 class Timeview:
@@ -11,5 +12,47 @@ class Timeview:
         self.min = self.timeline.min
         self.max = self.timeline.max
 
-    def contains(self, d: date) -> bool:
+    def contains(self, timelike: Union[date, TimeReference, EventRecord]) -> bool:
+        if type(timelike) is date:
+            return self.contains_date(timelike)
+        elif type(timelike) is TimeReference:
+            return self.contains_reference(timelike)
+        elif type(timelike) is EventRecord:
+            return self.contains_record(timelike)
+
+    def contains_date(self, d: date) -> bool:
+        """
+        Determine whether the date `d` is currently visible to the Timeview.
+        Args:
+            d: A date to compare
+
+        Returns:
+            True if `d` falls within the Timeview's purview, false otherwise.
+        """
         return self.min <= d <= self.max
+
+    def contains_reference(self, tr: TimeReference) -> bool:
+        """
+        Determine whether the given time reference is visible to the Timeview
+        Args:
+            tr: A normalized TimeReference (min and max must not be None).
+
+        Returns:
+            True if the TimeReference should be visible in this Timeview, false otherwise.
+        """
+        if type(tr.max) is date and tr.max < self.min \
+                or type(tr.min) is date and tr.min > self.max:
+            return False
+
+        return True
+
+    def contains_record(self, rec: EventRecord) -> bool:
+        """
+        Determine whether the provided EventRecord is visible to this Timeview.
+        Args:
+            rec: A normalized EventRecord
+
+        Returns:
+            True if the EventRecord would be at least partly visible within this Timeview, else False.
+        """
+        return self.contains_reference(rec.start) or self.contains_reference(rec.end)
--- test/test_timeview.py
+++ test/test_timeview.py
@@ -1,8 +1,8 @@
-
+import math
 import unittest
 from datetime import date
 
-from data_types import Timeline, Timeview, EventRecord
+from data_types import Timeline, Timeview, TimeReference, EventRecord
 
 
 class TestTimeview(unittest.TestCase):
@@ -30,6 +30,81 @@ class TestTimeview(unittest.TestCase):
         self.assertEqual(view.max, tl.max)
 
     def test_contains(self):
+
+        # Arrange
+        tl = Timeline()
+        tl.load_from_record_list(self.record_list)
+
+        # Act
+        view = Timeview(tl)
+
+        # Assert
+        for rr in tl.get_records().values():
+            self.assertTrue(view.contains(rr.start.min))
+
+        early_date = date(year=1970, month=8, day=16)
+        late_date = date(year=2040, month=6, day=6)
+        for dd in [early_date, late_date]:
+            self.assertFalse(view.contains(dd))
+
+    def test_contains_reference(self):
+
+        # Arrange
+        tl = Timeline()
+        tl.load_from_record_list(self.record_list)
+
+        # The timeline goes from 17 Aug 1970 to 5 Jun 2040.
+        # The view contains the whole span by default, so we'll test against that.
+        # Generate some new records that fall inside and outside that range.
+        more_records = [
+            {'name': 'in fixed point',
+             'start': '4 Apr 2020',
+             },
+            {'name': 'in fixed range',
+             'start': '1970',
+             },
+            {'name': 'in half infinity',
+             'start_after': 'June 2000',
+             },
+            {'name': 'in half -infinity',
+             'start_before': 'Sep 1970',
+             },
+            {'name': 'in unbounded'
+             },
+
+            {'name': 'out fixed point',
+             'start': '4 June 1970',
+             },
+            {'name': 'out fixed range',
+             'start': 'July 2040',
+             },
+            {'name': 'out half infinity',
+             'start_after': 'July 2040',
+             },
+            {'name': 'out half -infinity',
+             'start_before': 'July 1970',
+             },
+            ]
+        tl2 = Timeline()
+        tl2.load_from_record_list(more_records)
+        contains_true = [event.start for event in tl2.get_records().values() if event.name.startswith('in')]
+        contains_false = [event.start for event in tl2.get_records().values() if event.name.startswith('out')]
+
+        # Act
+        view = Timeview(tl)
+
+        # Assert
+        for tr in contains_true:
+            self.assertTrue(view.contains(tr))
+        for tr in contains_false:
+            self.assertFalse(view.contains(tr))
+
+        early_date = date(year=1970, month=8, day=16)
+        late_date = date(year=2040, month=6, day=6)
+        for dd in [early_date, late_date]:
+            self.assertFalse(view.contains(dd))
+
+    def test_contains_record(self):
 
         # Arrange
         tl = Timeline()
